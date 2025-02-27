0a35899cadd267b876fec80f2d8accb2b9576374
Tristen Lawrence <tristen.lawrence@yale.edu>
2025-02-27 15:26:43 -0500

    update

--- cli/update_container.go
+++ cli/update_container.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"os"
+	"strings"
 
 	"github.com/YaleSpinup/spinup-cli/pkg/spinup"
 	log "github.com/sirupsen/logrus"
@@ -14,14 +15,16 @@ import (
 var (
 	redeployContainerCmd bool
 	scaleContainerCmd    int64
-	imageTagCmd          string
+	containerNameCmd     string
+	containerTagCmd      string
 )
 
 func init() {
 	updateCmd.AddCommand(updateContainerCmd)
 	updateContainerCmd.PersistentFlags().BoolVarP(&redeployContainerCmd, "redeploy", "r", false, "Redeploy with the current configuration.")
 	updateContainerCmd.PersistentFlags().Int64Var(&scaleContainerCmd, "scale", 0, "Scale the container service")
-	updateContainerCmd.PersistentFlags().StringVar(&imageTagCmd, "tag", "", "Update the container image tag")
+	updateContainerCmd.PersistentFlags().StringVar(&containerNameCmd, "container", "", "The name of the container to update")
+	updateContainerCmd.PersistentFlags().StringVar(&containerTagCmd, "tag", "", "The new image tag for the container")
 }
 
 var updateContainerCmd = &cobra.Command{
@@ -38,8 +41,9 @@ var updateContainerCmd = &cobra.Command{
 		var j []byte
 		var err error
 
-		if cmd.Flags().Changed("tag") {
-			if j, err = updateContainerImage(updateParams, updateResource, imageTagCmd, redeployContainerCmd); err != nil {
+		// Check if container update flags are set
+		if cmd.Flags().Changed("container") && cmd.Flags().Changed("tag") {
+			if j, err = updateContainerImageTag(updateParams, updateResource, containerNameCmd, containerTagCmd, redeployContainerCmd); err != nil {
 				return err
 			}
 		} else if cmd.Flags().Changed("scale") {
@@ -50,6 +54,8 @@ var updateContainerCmd = &cobra.Command{
 			if j, err = redeployContainer(updateParams, updateResource); err != nil {
 				return err
 			}
+		} else if cmd.Flags().Changed("container") || cmd.Flags().Changed("tag") {
+			return errors.New("both --container and --tag must be specified to update the container image")
 		}
 
 		f := bufio.NewWriter(os.Stdout)
@@ -108,8 +114,8 @@ func scaleContainer(params map[string]string, resource *spinup.Resource, scale i
 	return []byte("OK\n"), nil
 }
 
-func updateContainerImage(params map[string]string, resource *spinup.Resource, tag string, force bool) ([]byte, error) {
-	log.Infof("updating container service image tag to %s", tag)
+func updateContainerImageTag(params map[string]string, resource *spinup.Resource, containerName, newTag string, forceRedeploy bool) ([]byte, error) {
+	log.Infof("updating container %s image tag to %s", containerName, newTag)
 
 	// Get the container service details
 	info := &spinup.ContainerService{}
@@ -117,24 +123,63 @@ func updateContainerImage(params map[string]string, resource *spinup.Resource, t
 		return []byte{}, err
 	}
 
-	// Create a copy of the container service update input with the existing settings
-	serviceInput := &spinup.ContainerServiceUpdateInput{
-		CapacityProviderStrategy: []*spinup.CapacityProviderStrategyInput{
+	// Create a copy of the task definition
+	taskDefinition := info.TaskDefinition
+	containerUpdated := false
+
+	// Loop through container definitions and update the specified container
+	for i, container := range taskDefinition.ContainerDefinitions {
+		if container.Name == containerName {
+			// Parse the current image to get repository and update the tag
+			imageParts := strings.Split(container.Image, ":")
+			if len(imageParts) < 2 {
+				return []byte{}, errors.New("current image format is not valid, expected repository:tag")
+			}
+			
+			// Update the image with the new tag
+			taskDefinition.ContainerDefinitions[i].Image = imageParts[0] + ":" + newTag
+			containerUpdated = true
+			break
+		}
+	}
+
+	if !containerUpdated {
+		return []byte{}, errors.New("container with name " + containerName + " not found in task definition")
+	}
+
+	// Create the update input
+	updateInput := &spinup.ContainerServiceUpdateInput{
+		ContainerDefinitions: taskDefinition.ContainerDefinitions,
+		PlatformVersion:      "LATEST",
+		DesiredCount:         info.DesiredCount,
+	}
+
+	// Add the capacity provider strategy if it exists
+	if len(info.CapacityProviderStrategy) > 0 {
+		capProviders := make([]*spinup.CapacityProviderStrategyInput, 0, len(info.CapacityProviderStrategy))
+		for _, cp := range info.CapacityProviderStrategy {
+			capProviders = append(capProviders, &spinup.CapacityProviderStrategyInput{
+				Base:             int64(cp.Base),
+				CapacityProvider: cp.CapacityProvider,
+				Weight:           int64(cp.Weight),
+			})
+		}
+		updateInput.CapacityProviderStrategy = capProviders
+	} else {
+		// Default to FARGATE_SPOT if no strategy is defined
+		updateInput.CapacityProviderStrategy = []*spinup.CapacityProviderStrategyInput{
 			{
 				Base:             1,
 				CapacityProvider: "FARGATE_SPOT",
 				Weight:           1,
 			},
-		},
-		DesiredCount:    info.DesiredCount,
-		PlatformVersion: "LATEST",
-		ImageTag:        tag,
+		}
 	}
 
 	input, err := json.Marshal(spinup.ContainerServiceWrapperUpdateInput{
-		Size:         resource.SizeID,
-		Service:      serviceInput,
-		ForceRedeploy: force || true, // Force redeploy when updating the image tag
+		Size:          resource.SizeID,
+		Service:       updateInput,
+		ForceRedeploy: forceRedeploy || true, // Always force redeploy when updating container image
 	})
 	if err != nil {
 		return []byte{}, err
@@ -142,7 +187,8 @@ func updateContainerImage(params map[string]string, resource *spinup.Resource, t
 
 	log.Debugf("putting input: %s", string(input))
 
-	if err = SpinupClient.PutResource(params, input, info); err != nil {
+	updatedInfo := &spinup.ContainerService{}
+	if err = SpinupClient.PutResource(params, input, updatedInfo); err != nil {
 		return []byte{}, err
 	}
 
