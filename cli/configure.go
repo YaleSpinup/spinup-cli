1559e0731734cee775fd93412c930731852cbc7e
Tristen Lawrence <tristen.lawrence@yale.edu>
2025-03-03 14:13:11 -0500

    more more fix

--- cli/configure.go
+++ cli/configure.go
@@ -1,22 +1,71 @@
-package spinup
+package cli
 
-// These are the updated struct definitions for the container.go file
+import (
+	"fmt"
+	"strings"
 
-type ContainerServiceWrapperUpdateInput struct {
-	ForceRedeploy bool                         `json:"force_redeploy"`
-	Service       *ContainerServiceUpdateInput `json:"service"`
-	Size          *FlexInt                     `json:"size_id"`
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var show bool
+
+func init() {
+	rootCmd.AddCommand(configureCmd)
+	configureCmd.Flags().BoolVar(&show, "show", false, "Display the current configurations")
 }
 
-type ContainerServiceUpdateInput struct {
-	CapacityProviderStrategy []*CapacityProviderStrategyInput `json:"capacity_provider_strategy,omitempty"`
-	ContainerDefinitions     []*ContainerDefinition           `json:"container_definitions,omitempty"`
-	DesiredCount             int64                            `json:"desired_count,omitempty"`
-	PlatformVersion          string                           `json:"platform_version,omitempty"`
-}
+var configureCmd = &cobra.Command{
+	Use:     "configure",
+	Aliases: []string{"config"},
+	Short:   "Configure Spinup CLI",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		log.Debug("Configuring...")
 
-type CapacityProviderStrategyInput struct {
-	Base             int64  `json:"base"`
-	CapacityProvider string `json:"capacity_provider"`
-	Weight           int64  `json:"weight"`
+		if show {
+			for k, v := range viper.AllSettings() {
+				fmt.Printf("%s:\t%+v\n", k, v)
+			}
+			return nil
+		}
+
+		var url, token, spaces string
+
+		fmt.Printf("URL [%s]: ", spinupURL)
+		fmt.Scanln(&url)
+		if url == "" {
+			url = spinupURL
+		}
+		viper.Set("url", url)
+
+		fmt.Printf("Token [%s]: ", spinupToken)
+		fmt.Scanln(&token)
+		if token == "" {
+			token = spinupToken
+		}
+		viper.Set("token", token)
+
+		fmt.Printf("Spaces [%s]: ", strings.Join(spinupSpaces, ","))
+		fmt.Scanln(&spaces)
+		if spaces == "" {
+			spaces = strings.Join(spinupSpaces, ",")
+		}
+
+		spaceNames := strings.Split(spaces, ",")
+		if spaces != "" {
+			viper.Set("spaces", spaceNames)
+		} else {
+			viper.Set("spaces", []string{})
+		}
+
+		log.Debugf("setting url %s, token %s, and spaces %+v", url, token, spaces)
+
+		if err := viper.WriteConfig(); err != nil {
+			return err
+		}
+
+		return nil
+	},
 }
